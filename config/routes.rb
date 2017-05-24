00dab734491c480d40af39ba59552c4e5994fecf
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-24 12:27:31 -0700

    Added projects route

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'contact', to: 'welcome#contact', as: 'contact'
+  get 'projects', to: 'welcome#projects', as: 'projects'
   get 'articles/tags/:tag', to: 'articles#index', as: 'tag'
 end
