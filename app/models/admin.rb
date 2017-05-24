302106755a19590a7d312d93db29544a44fcf014
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-24 00:26:08 -0700

    Only 1 admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,16 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :articles
+    has_many :articles
+    validate :one_admin
+
+    private
+
+    def one_admin
+        if (not Admin.count == 0)
+            errors.add(:id, "There can only be 1 administrator")
+        end
+    end
 end
