cc446bfc6e20afabba87a45675c1259745a05c84
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-26 10:14:20 -0700

    Developed collapsible subcontent for experience and extracurriculars

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  get 'contact', to: 'welcome#contact', as: 'contact'
   get 'projects', to: 'welcome#projects', as: 'projects'
+  get 'art', to: 'weclome#art', as: 'art'
+  get 'design', to: 'welcome#design', as: 'design'
+  get 'contact', to: 'welcome#contact', as: 'contact'
   get 'articles/tags/:tag', to: 'articles#index', as: 'tag'
 end
