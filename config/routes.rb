6b33bf0d0a8e0ff35e428f94f3fd562b2f7518b8
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-27 11:32:28 -0700

    styled comments section

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
 
   get 'projects', to: 'welcome#projects', as: 'projects'
   get 'freelancing', to: 'welcome#freelancing', as: 'freelancing'
-  get 'art', to: 'weclome#art', as: 'art'
-  get 'design', to: 'welcome#design', as: 'design'
+  get 'art-design', to: 'weclome#art-design', as: 'art_design'
   get 'contact', to: 'welcome#contact', as: 'contact'
   get 'articles/tags/:tag', to: 'articles#index', as: 'tag'
 end
