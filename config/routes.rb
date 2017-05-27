34162591f3c0946f2a35ee4b1906ea89076e90b0
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-27 14:21:57 -0700

    Added font styling

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'projects', to: 'welcome#projects', as: 'projects'
   get 'freelancing', to: 'welcome#freelancing', as: 'freelancing'
-  get 'art-design', to: 'weclome#art-design', as: 'art_design'
+  get 'art-design', to: 'welcome#art-design', as: 'art_design'
   get 'contact', to: 'welcome#contact', as: 'contact'
   get 'articles/tags/:tag', to: 'articles#index', as: 'tag'
 end
