18b118b0f4e85cdd546a89ce7ddbe85d7378ec0d
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-27 18:07:42 -0700

    fixed regex bug

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
     attribute :name, validate: true
-    attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]{2,})\z/i
+    attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
     attribute :message, validate: true
     attribute :nickname, captcha: true
 
