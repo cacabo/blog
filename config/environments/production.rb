287efab0e1e67a28de8cb2b7c66973500f725309
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-28 17:17:04 -0700

    fixed misspelling in config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   end
 
   # Setting up sendgrid and emailing
-  config.acion_mailer.default_url_options = { host: 'https://cameroncabo.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://cameroncabo.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
