c9906c0c5c651ab71c1c8cec142e27bfa63b14e1
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-28 20:27:52 -0700

    updated routing

--- app/assets/javascripts/collapser.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.collapser').click(function() {
-        var $icon = $(this).find('i');
-        $icon.toggleClass('rotate');
-    });
-});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
-  get 'welcome/index'
 
   resources :articles do
     resources :comments
