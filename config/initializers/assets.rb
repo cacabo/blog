83b2a0e9275b95a44feb3c597bf903d3b8141c05
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-03 23:56:10 -0700

    Added ttt style and js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( project-styles/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( project-styles/ttt-style.css )
+Rails.application.config.assets.precompile += %w( project-js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( project-js/ttt.js )
