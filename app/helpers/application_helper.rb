9c3925fa652e463c73ac1e47581b934c479871dc
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-19 08:41:31 -0700

    updated title, description, and keyword formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def title(page_title)
         content_for(:title) { page_title }
     end
+
+    def description(page_description)
+        content_for(:description) { page_description }
+    end
+
+    def keywords(page_keywords)
+        content_for(:keywords) { page_keywords }
+    end
 end
